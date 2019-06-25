aac6b66e95417107b985b47fe86e70ea804d9d69
Nick <nick@Nicks-MacBook-Pro.local>
2019-06-25 22:17:05 +0800

    remove rotation

--- components/cookie.js
+++ components/cookie.js
@@ -2,25 +2,6 @@ import React, { Component } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet, Image } from 'react-native';
 
 export default class CookieButton extends React.Component {
-
-  spinValue = new Animated.Value(0)
-
-  // First set up animation 
-  Animated.timing(
-      this.spinValue,
-    {
-      toValue: 1,
-      duration: 3000,
-      easing: Easing.linear
-    }
-  ).start()
-
-  // Second interpolate beginning and end values (in this case 0 and 1)
-  const spin = this.spinValue.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0deg', '360deg']
-  })
-
   render() {
     return (
       <View>
@@ -47,6 +28,5 @@ const styles = StyleSheet.create({
   cookie: {
     width: '100%',
     height: 300,
-    transform: [{rotate: spin}]
   },
 });
